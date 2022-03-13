850af02995290150f2e36f3be6dbcc0076b7eff9
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-13 16:45:22 -0400

    Added a scope name mangling method for better error messaging. Add var will now also return a reference to the variable, and lookup_var returns None instead of throwing an exception

--- pifive_module/transpiler/scope.py
+++ pifive_module/transpiler/scope.py
@@ -1,19 +1,28 @@
 class Variable:
-  def __init__(self, name, type, reg, offset):
+  def __init__(self, name, type=None, reg=None, offset=None):
     self.name = name
     self.type = type
     self.reg = reg
     self.offset = offset
 
 class Scope:
-  def __init__(self, parent_scope=None):
+  def __init__(self, name, parent_scope=None):
+    self.name = name
     self.parent_scope : Scope = parent_scope
     self._variables = {}
     self._functions = {} 
   
+  def nm_scope(self):
+    # Return name mangled scope
+    if self.parent_scope:
+      return f"{self.parent_scope.nm_scope()}/{self.name}"
+    else:
+      return self.name
+
   def add_var(self, var : Variable):
     if var.name not in self._variables:
       self._variables[var.name] = var
+    return self._variables[var.name]
   
   def lookup_var(self, var_name : str):
     if var_name in self._variables:
@@ -21,4 +30,4 @@ class Scope:
     elif self.parent_scope is not None:
       return self.parent_scope.lookup_var(var_name)
     else:
-      raise(f"Variable {var_name} does not exist in the current scope!")
+      return None
