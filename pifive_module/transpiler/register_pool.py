b461f206184a9e06f44303679d804f3aade1256e
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-12 21:58:49 -0500

    Implemented a Reg, RegType, and RegPool to allocate available registers

---
+++ pifive_module/transpiler/register_pool.py
@@ -0,0 +1,41 @@
+from .registers import Reg, RegType
+
+class RegPool:
+  def __init__(self):
+    # Initially all registers are available
+    self.available_regs = [True for i in range(len(Reg))]
+  
+  def get_next_reg(self, reg_type : RegType):
+    # Get next available register of a particular type
+    for reg in reg_type.value:
+      print(reg.name)
+      if self.available_regs[reg.value]:
+        self.available_regs[reg.value] = False
+        return reg
+    return None
+  
+  def restore_reg(self, reg : Reg):
+    # Make a register available again
+    self.available_regs[reg.value] = True
+  
+  def take_reg(self, reg : Reg):
+    # Make a register unavailable
+    self.available_regs[reg.value] = False
+  
+  def idx_of_regs_in_use(self):
+    regs_in_use = []
+    for idx, reg in enumerate(self.available_regs):
+      if not reg:
+        regs_in_use.append(idx)
+    return regs_in_use
+
+if __name__ == "__main__":
+  reg_pool = RegPool()
+  temp_reg_1 = reg_pool.get_next_reg(RegType.temp_regs)
+  temp_reg_2 = reg_pool.get_next_reg(RegType.temp_regs)
+  save_reg_1 = reg_pool.get_next_reg(RegType.arg_regs)
+  print(f"temp_reg_1: name {temp_reg_1.name}, value {temp_reg_1.value}")
+  print(f"temp_reg_2: name {temp_reg_2.name}, value {temp_reg_2.value}")
+  print(f"save_reg_1: name {save_reg_1.name}, value {save_reg_1.value}")
+  print("Regs in use")
+  print(reg_pool.idx_of_regs_in_use())
---
+++ pifive_module/transpiler/registers.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+class Reg(Enum):
+  zero = 0
+  ra = 1
+  sp = 2
+  gp = 3
+  tp = 4
+  t0 = 5
+  t1 = 6
+  t2 = 7
+  s0 = 8
+  s1 = 9
+  a0 = 10
+  a1 = 11
+  a2 = 12
+  a3 = 13
+  a4 = 14
+  a5 = 15
+  a6 = 16
+  a7 = 17
+  s2 = 18
+  s3 = 19
+  s4 = 20
+  s5 = 21
+  s6 = 22
+  s7 = 23
+  s8 = 24
+  s9 = 25
+  s10 = 26
+  s11 = 27
+  t3 = 28
+  t4 = 29
+  t5 = 30
+  t6 = 31
+
+class RegType(Enum):
+  temp_regs = [
+    Reg.t0,
+    Reg.t1,
+    Reg.t2,
+    Reg.t3,
+    Reg.t4,
+    Reg.t5,
+    Reg.t6
+  ]
+  saved_regs = [
+    Reg.s0,
+    Reg.s1,
+    Reg.s2,
+    Reg.s3,
+    Reg.s4,
+    Reg.s5,
+    Reg.s6,
+    Reg.s7,
+    Reg.s8,
+    Reg.s9,
+    Reg.s10,
+    Reg.s11
+  ]
+  arg_regs = [
+    Reg.a0,
+    Reg.a1,
+    Reg.a2,
+    Reg.a3,
+    Reg.a4,
+    Reg.a5,
+    Reg.a6,
+    Reg.a7
+  ]
+
+def print_reg_type_info(reg_type : RegType):
+  print(f"\"{reg_type.name}\" info:")
+  for reg in reg_type.value:
+    print(f"name {reg.name}, value {reg.value}") 
+  print("")
+
+if __name__ == "__main__":
+  print_reg_type_info(RegType.temp_regs)
+  print_reg_type_info(RegType.saved_regs)
+  print_reg_type_info(RegType.arg_regs)
+  print(f"Length of registers {len(Reg)}")
