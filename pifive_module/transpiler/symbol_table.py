68f3a909cd6486a12d57ca70c28db6387d0c9c34
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-21 13:31:15 -0400

    Added a symbol type which Variable and Function both inherit from. Additionally added a symbol table

---
+++ pifive_module/transpiler/symbol_table.py
@@ -0,0 +1,31 @@
+from .instruction_maker import InstructionMaker
+from .symbols import Symbol, SymbolKind
+from .registers import Reg
+
+class SymbolTable:
+  def __init__(self):
+    '''frame -> symbol_list'''
+    self.symbol_table : dict[str,list[Symbol]] = {}
+
+  def free_and_save_reg(self, frame : str, instr : InstructionMaker) -> Reg:
+    pass
+
+  def free_and_save_all_regs(self, frame : str, instr : InstructionMaker):
+    pass
+
+  def lookup_symbol(self, symbol_name : str, frame : str, kind : SymbolKind):
+    if frame not in self.symbol_table:
+      raise RuntimeError(f"frame {frame} does not exist!")
+
+    for symbol in self.symbol_table[frame]:
+      if symbol.name == symbol_name and symbol.kind == kind:
+        return symbol
+
+    raise RuntimeWarning(f'Symbol "{symbol_name}" does not exist in frame "{frame}"')
+
+  def add_symbol(self, frame : str, symbol : Symbol):
+    if frame not in self.symbol_table:
+      self.symbol_table[frame] = [symbol]
+    else:
+      self.symbol_table[frame].append(symbol)
+
---
+++ pifive_module/transpiler/symbols.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from .registers import Reg
+
+class SymbolKind(Enum):
+  variable = 'variable'
+  function = 'function'
+
+class Symbol:
+  def __init__(self, name, kind : SymbolKind):
+    self.name = name
+    self.kind = kind
+
+class Variable(Symbol):
+  def __init__(self, name, type=None, reg=None, offset=None):
+    super().__init__(name, kind=SymbolKind.variable)
+    self.type = type
+    self.reg : Reg = reg
+    self.offset = offset
+
+class Function(Symbol):
+  def __init__(self, name, args, ret_reg=None):
+    super().__init__(name, kind=SymbolKind.function)
+    self.ret : Reg = ret_reg
+    self.args : list[Reg] = args
