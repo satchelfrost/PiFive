8ca028516e0b0864bf398bacd17cae52dcbb58be
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-21 13:28:47 -0400

    Scope no longer returns variables and Variable is now part of symbols file

--- pifive_module/transpiler/scope.py
+++ pifive_module/transpiler/scope.py
@@ -1,29 +1,19 @@
-class Variable:
-  def __init__(self, name, type=None, reg=None, offset=None):
-    self.name = name
-    self.type = type
-    self.reg = reg
-    self.offset = offset
+from .symbol_table import SymbolTable
+from .symbols import Variable
 
 class Scope:
-  def __init__(self, name, parent_scope=None):
-    self.name = name
+  def __init__(self, name='global', parent_scope=None):
+    self.frame = name
     self.parent_scope : Scope = parent_scope
     self._variables = {}
-    self._functions = {} 
-  
-  def nm_scope(self):
-    # Return name mangled scope
-    if self.parent_scope:
-      return f"{self.parent_scope.nm_scope()}/{self.name}"
-    else:
-      return self.name
+    self._functions = {}
 
   def add_var(self, var : Variable):
     if var.name not in self._variables:
       self._variables[var.name] = var
-    return self._variables[var.name]
-  
+    else:
+      raise RuntimeError(f'Variable {var.name} already exists in the current scope!')
+
   def lookup_var(self, var_name : str):
     if var_name in self._variables:
       return self._variables[var_name]
