251866e6054d78042b4f00ac7a9a7fa67da5be2d
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-23 23:24:18 -0400

    Added assembly comments, color printing, and BinOp enum

--- pifive_module/transpiler/instruction_maker.py
+++ pifive_module/transpiler/instruction_maker.py
@@ -1,7 +1,8 @@
 from .registers import Reg
 from enum import Enum
+from colorama import Fore
 
-class Binop(Enum):
+class BinOp(Enum):
   ADD = 'add' 
   SUB = 'sub'
   AND = 'and'
@@ -9,6 +10,24 @@ class Binop(Enum):
   XOR = 'xor'
   MUL = 'mul'
   DIV = 'div'
+
+  def to_english(self):
+    if self.name == BinOp.ADD.name:
+      return "Add"
+    elif self.name == BinOp.SUB.name:
+      return "Subtract"
+    elif self.name == BinOp.AND.name:
+      return "AND"
+    elif self.name == BinOp.OR.name:
+      return "OR"
+    elif self.name == BinOp.XOR.name:
+      return "XOR"
+    elif self.name == BinOp.MUL.name:
+      return "Multiply"
+    elif self.name == BinOp.DIV.name:
+      return "Divide"
+    else:
+      raise RuntimeError(f'binop {self.name} has no english equivalent.')
 
 class InstructionMaker:
   def __init__(self, comments_on=False):
@@ -19,8 +38,12 @@ class InstructionMaker:
     self.instr_buffer = []
 
   def print(self):
-    print("\n".join(self.instr_buffer))
-  
+    for line in self.instr_buffer:
+      if "#" in line:
+        print(Fore.CYAN + line)
+      else:
+        print(Fore.LIGHTWHITE_EX + line)
+
   def load_imm(self, reg : Reg, value):
     self.instr_buffer.append(f"\tli {reg.name}, {value}")
 
@@ -31,7 +54,7 @@ class InstructionMaker:
     self.instr_buffer.append(f"\taddi sp, sp, -8")
     self.instr_buffer.append(f"\tli {reg.name}, {value}")
     self.instr_buffer.append(f"\tsd {reg.name}, 0(sp)")
-  
+
   def push_reg(self, reg : Reg):
     self.instr_buffer.append(f"\taddi sp, sp, -8")
     self.instr_buffer.append(f"\tsd {reg.name}, 0(sp)")
@@ -42,12 +65,31 @@ class InstructionMaker:
 
   def binop(self, binop : str, dst : Reg, src_1 : Reg, src_2 : Reg):
     self.instr_buffer.append(f"\t{binop} {dst.name}, {src_1.name}, {src_2.name}")
-  
+
+
+  #### Assembly comment section - not actual instructions! ####
   def comment(self, comment):
     if self.comments_on:
       self.instr_buffer.append(f"\t# {comment}")
 
+  def comment_reg_free(self, reg : Reg):
+    self.comment(f'Register "{reg.name}" freed')
+
+  def comment_push_val(self, value : str):
+    self.comment(f'Push value "{value}" to the stack')
+
+  def comment_pop(self, reg : Reg):
+    self.comment(f'Activate "{reg.name}" by popping stack.')
+
+  def comment_binop(self, binop : BinOp, reg : Reg):
+    self.comment(f'{binop.to_english()} result and store into "{reg.name}"')
+
+  def comment_binop_push(self, binop : BinOp, reg : Reg):
+    self.comment(f'Push {binop.to_english().lower()}-result from "{reg.name}" to stack')
+
+  def comment_assign(self, name : str):
+      self.comment(f'Target assignment variable "{name}".')
+
   def newline(self):
     if self.comments_on:
       self.instr_buffer.append("")
-
