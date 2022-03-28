3a4eb11e03bf7d508f0bf562a1e208267524cd99
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-28 07:53:24 -0400

    Added some unit tests for complex binops, as well as all of the simple binops

--- pifive_module/tests/testTranspiler.py
+++ pifive_module/tests/testTranspiler.py
@@ -5,7 +5,7 @@ from ast import parse
 class TestTranspiler(unittest.TestCase):
   rv = RISCV_Transpiler()
 
-  def transform(self, src_in, src_out):
+  def transforms(self, src_in, src_out):
     self.rv.reset()
     node = parse("\n".join(src_in))
     self.rv.transpile(node)
@@ -23,7 +23,7 @@ class TestTranspiler(unittest.TestCase):
       "\tld t0, 0(sp)",
       "\taddi sp, sp, 8"
     ]
-    self.transform(src_in, src_out)
+    self.transforms(src_in, src_out)
 
   def test_successive_assign(self):
     src_in = [
@@ -42,7 +42,7 @@ class TestTranspiler(unittest.TestCase):
       "\tld t1, 0(sp)",
       "\taddi sp, sp, 8"
     ]
-    self.transform(src_in, src_out)
+    self.transforms(src_in, src_out)
 
   def test_reassign(self):
     src_in = [
@@ -61,15 +61,180 @@ class TestTranspiler(unittest.TestCase):
       "\tsd t1, 0(sp)",
       "\tld t1, 0(sp)",
       "\taddi sp, sp, 8",
-      "\tmv t0, t1"
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
     ]
-    self.transform(src_in, src_out)
+    self.transforms(src_in, src_out)
 
-  # def test_reassign(self):
-  #   src_in = [
-  #     "a = 1 + 2 + 3",
-  #   ]
-  #   src_out = [
-  #     "",
-  #   ]
-  #   self.transform(src_in, src_out)
+  def test_add_binop(self):
+    src_in, src_out = self.simple_binop("1", "+", "41", "add")
+    self.transforms(src_in, src_out)
+
+  def test_mul_binop(self):
+    src_in, src_out = self.simple_binop("1", "*", "41", "mul")
+    self.transforms(src_in, src_out)
+
+  def test_div_binop(self):
+    src_in, src_out = self.simple_binop("1", "/", "41", "div")
+    self.transforms(src_in, src_out)
+
+  def test_sub_binop(self):
+    src_in, src_out = self.simple_binop("1", "-", "41", "sub")
+    self.transforms(src_in, src_out)
+
+  def test_and_binop(self):
+    src_in, src_out = self.simple_binop("1", "&", "41", "and")
+    self.transforms(src_in, src_out)
+
+  def test_or_binop(self):
+    src_in, src_out = self.simple_binop("1", "|", "41", "or")
+    self.transforms(src_in, src_out)
+
+  def test_xor_binop(self):
+    src_in, src_out = self.simple_binop("1", "^", "41", "xor")
+    self.transforms(src_in, src_out)
+
+  def simple_binop(self, left : str, op : str, right : str, instr : str):
+    src_in = [
+      f"a = {left} {op} {right}"
+    ]
+    src_out = [
+      "\taddi sp, sp, -8",
+      f"\tli t0, {left}",
+      "\tsd t0, 0(sp)",
+      "\taddi sp, sp, -8",
+      f"\tli t0, {right}",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      f"\t{instr} t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8"
+    ]
+    return src_in, src_out
+
+
+  def test_simple_binop_with_load(self):
+    src_in = [
+      "b = 41",
+      "a = 1 - b"
+    ]
+    src_out = [
+      "\taddi sp, sp, -8",
+      "\tli t0, 41",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\taddi sp, sp, -8",
+      "\tli t1, 1",
+      "\tsd t1, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tsub t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8"
+    ]
+    self.transforms(src_in, src_out)
+
+  def test_complex_binop(self):
+    src_in = [
+      "a = 1 - 41 + 3 * 5"
+    ]
+    src_out = [
+      "\taddi sp, sp, -8",
+      "\tli t0, 1",
+      "\tsd t0, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tli t0, 41",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tsub t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tli t0, 3",
+      "\tsd t0, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tli t0, 5",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tmul t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tadd t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8"
+    ]
+    self.transforms(src_in, src_out)
+
+  def test_complex_binop_with_load(self):
+    src_in = [
+      "b = 103",
+      "a = 41 + 3 * 5 - b"
+    ]
+    src_out = [
+      "\taddi sp, sp, -8",
+      "\tli t0, 103",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\taddi sp, sp, -8",
+      "\tli t1, 41",
+      "\tsd t1, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tli t1, 3",
+      "\tsd t1, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tli t1, 5",
+      "\tsd t1, 0(sp)",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t2, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tmul t2, t2, t1",
+      "\taddi sp, sp, -8",
+      "\tsd t2, 0(sp)",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t2, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tadd t2, t2, t1",
+      "\taddi sp, sp, -8",
+      "\tsd t2, 0(sp)",
+      "\taddi sp, sp, -8",
+      "\tsd t0, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tld t1, 0(sp)",
+      "\taddi sp, sp, 8",
+      "\tsub t1, t1, t0",
+      "\taddi sp, sp, -8",
+      "\tsd t1, 0(sp)",
+      "\tld t0, 0(sp)",
+      "\taddi sp, sp, 8"
+    ]
+    self.transforms(src_in, src_out)
