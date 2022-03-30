1eed37f75c03a94290cda28e3731366ed8e49611
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-03-30 17:11:18 -0400

    Added a scope_id, and a label counter

--- pifive_module/transpiler/scope.py
+++ pifive_module/transpiler/scope.py
@@ -1,12 +1,20 @@
-from .symbol_table import SymbolTable
 from .symbols import Variable
 
 class Scope:
-  def __init__(self, name='global', parent_scope=None):
+  def __init__(self, name='global', parent=None):
     self.frame = name
-    self.parent_scope : Scope = parent_scope
+    self.parent : Scope = parent
     self._variables = {}
     self._functions = {}
+    self._label_counter = -1
+    if parent is not None:
+      self.scope_id = self.parent.scope_id + 1
+    else:
+      self.scope_id = 0
+
+  def get_next_label_number(self):
+    self._label_counter += 1
+    return self._label_counter
 
   def add_var(self, var : Variable):
     if var.name not in self._variables:
@@ -17,7 +25,7 @@ class Scope:
   def lookup_var(self, var_name : str):
     if var_name in self._variables:
       return self._variables[var_name]
-    elif self.parent_scope is not None:
-      return self.parent_scope.lookup_var(var_name)
+    elif self.parent is not None:
+      return self.parent.lookup_var(var_name)
     else:
       return None
