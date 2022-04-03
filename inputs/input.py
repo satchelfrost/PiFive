69188229a590a1d68fc74aad76a26ead4b326895
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-04-03 14:09:04 -0400

    This file always changes

--- inputs/input.py
+++ inputs/input.py
@@ -1,72 +1,38 @@
-#def function(a, b):
-#  c = a + b
-#  return c
-
-# function(1, function(2, 3))
-# a = 45
-# b = 54
-# function(a, b)
-
 # def factorial(n):
 #   if n == 0:
 #     return 1
-#   # else:
 #   return n * factorial(n-1)
 
 # def main():
 #   a = factorial(3 + 4) * 123
 #   return 0
 
-# def factorial(n):
-#   if n == 0:
-#     return 1
-#   return n * factorial(n - 1)
-
+# scope_0 = 1
 # def main():
-#   factorial(1)
-#   return 0
+#   scope_1 = 1 + scope_0 
+#   if scope_0 < scope_1:
+#     scope_2 = 1 + scope_1 + scope_0
+#   else:
+#     scope_z = 1 + scope_0 # Error?
+  # return scope_z
 
-# scope_0 = 0
-# def main():
-#   scope_1 = 1 + 1
-  
-#   if 2 < 3:
-#     scope_2 = 3
-#   if 2 > 3:
-#     scope_3  = 4
-
+# c = 5000
+# if 23 < 25:
+#   c = 100
+# elif 23 < 24:
+#   a = 399
+# else:
+#   a = 200
+# a = c
 
 # i = 0
 # while i < 10:
 #   i = i + 1
-  # a = 0
-# b = a
 
-# def loop_n(n):
-#   while n < 10:
-#     n = n + 1
-#   return 0
-
-# def main():
-#   a = 10
-#   loop_n(a)
-# def plus_one(n):
-#   return n + 1
-
-# def main():
-#   a = plus_one(2)
-#   return 0
-
-# 32 < 34
-
-# if 1 < 2:
-#   a = 1
-#   b = 2
-#   c = 3
-#   d = 4 
-#   e = 5
-#   f = 6
-#   g = 7
-#   h = 9
-# a = 3
-# h = 8
+# def sum(n):
+#   sum = n
+#   i = 0
+#   while i < 10:
+#     i = i + 1
+#     sum = sum + 1
+#   return sum
