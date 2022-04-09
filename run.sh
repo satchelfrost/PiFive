48eb09cbc27a6ea3e0e8f7d83b8040c62690d352
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-04-09 12:39:37 -0400

    Restructured some files

--- run.sh
+++
@@ -1,2 +0,0 @@
-python3 pifive.py run -i inputs/input.py -p -c # Compiles to console (assembly comments on)
-# python3 pifive.py run -i inputs/input.py -p # Compiles to console (assembly comments off)
