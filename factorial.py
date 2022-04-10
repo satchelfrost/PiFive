fc2a39b1dd9deb29b117c31be7e297086f2b24eb
Reese Gallagher <reese.gallagher@infinitasinc.com>
2022-04-10 12:02:32 -0400

    File always changes

--- factorial.py
+++ factorial.py
@@ -1,35 +1,16 @@
-# def factorial(n):
-#   if n == 0:
-#     return 1
-#   return n * factorial(n-1)
+def factorial(n):
+  if n == 0:
+    return 1
+  return n * factorial(n-1)
 
-# def main():
-#   i = 0
-#   while i < 10:
-#     a = factorial(3 + 4) * 123
-#     print(a)
-#     i = i + 1
-#   return 0
-
-# def main():
-#   a = factorial(0)
-#   print(a)
-#   return 0
 def main():
   i = 0
+  sum = 0
   while i < 10:
+    a = factorial(3 + 4) * 123
+    sum = sum + a
+    print(sum)
     i = i + 1
   return 0
 
 main()
-
-# def main():
-#   a = 1
-#   b = 2
-#   c = 3
-#   d = 4
-#   e = 5
-#   f = 6
-#   g = 7
-#   h = 8
-#   i = 9
